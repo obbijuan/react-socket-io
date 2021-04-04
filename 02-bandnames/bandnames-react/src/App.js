a2d93522158edff5f6d77c892b9e45d3e693670e
obbijuan <juan.garces@outlook.com>
2021-04-03 23:30:04 -0300

    Configure socket.io in react

--- 02-bandnames/bandnames-react/src/App.js
+++ 02-bandnames/bandnames-react/src/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
 
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports : ['websocket']
+  });
+  return socket;
+}
+
+
 function App() {
+
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+
+  useEffect(() => {
+
+    setOnline(socket.connected);
+
+  }, [socket])
+
+
+  useEffect(() => {
+
+    socket.on('connect', () => {
+      setOnline(true);
+    })
+
+  }, [socket])
+
+
+  useEffect(() => {
+
+    socket.on('disconnect', () => {
+      setOnline(false);
+    })
+
+  }, [socket])
+
+
   return (
     <div className="container">
       <div className="alert">
         <p>
           Service Status:
-          <span className="text-success"> Online</span>
-          <span className="text-danger"> Offline</span>
+          {
+            online
+              ? <span className="text-success"> Online</span>
+              : <span className="text-danger"> Offline</span>
+          }
         </p>
       </div>
 
