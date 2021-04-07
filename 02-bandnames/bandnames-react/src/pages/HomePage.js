e88972b87e554c916ef13f549c123387018ec957
obbijuan <juan.garces@outlook.com>
2021-04-06 23:27:22 -0400

    Chart.js added

--- 02-bandnames/bandnames-react/src/pages/HomePage.js
+++ 02-bandnames/bandnames-react/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import { SocketContext } from '../context/SocketContext';
 
 import { BandAdd } from '../components/BandAdd';
 import { BandList } from '../components/BandList';
+import { BandChart } from '../components/BandChart';
 
 
 
@@ -27,6 +28,12 @@ function HomePage() {
 
       <h1>BandNames</h1>
       <hr />
+
+      <div className="row">
+        <div className="col">
+          <BandChart/>
+        </div>
+      </div>
       
       <div className="row">
         <div className="col-8">
