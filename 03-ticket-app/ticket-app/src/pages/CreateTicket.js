49a9f7b26ad7589ebaea72d3b61a122145e2f0a4
obbijuan <juan.garces@outlook.com>
2021-04-11 12:05:21 -0400

    Create Ticket screen added

--- 03-ticket-app/ticket-app/src/pages/CreateTicket.js
+++ 03-ticket-app/ticket-app/src/pages/CreateTicket.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Button, Col, Row, Typography } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
 
 export const CreateTicket = () => {
+
+    const newTicket = () => {
+        console.log('newTicket');
+    }
+
     return (
-        <div>
-            CreateTicket
-        </div>
+        <>
+            <Row>
+                <Col span={ 14 } offset={ 4 } align="center">
+                    <Title level={ 3 }>
+                        Press the button for a new ticket
+                    </Title>
+
+                    <Button
+                        type="primary"
+                        shape="round"
+                        icon={ <DownloadOutlined /> }
+                        size="large"
+                        onClick={ newTicket }
+                    >
+                        New Ticket
+
+                    </Button>
+                </Col>
+            </Row>
+
+            <Row style={{ marginTop: 100 }}>
+                <Col span={ 14 } offset={ 4 } align="center">
+                    <Text level={ 2 }>
+                        Your Number
+                    </Text>
+                    <br />
+                    <Text type="success" style={{ fontSize: 55 }}>
+                        55
+                    </Text>
+                </Col>
+            </Row>
+        </>
     )
 }
