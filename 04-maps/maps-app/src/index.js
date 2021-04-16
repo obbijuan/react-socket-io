2cb5c9e3bd237553264eb5c365b7fab2ddd015a7
obbijuan <juan.garces@outlook.com>
2021-04-16 11:55:12 -0400

    Project start: maps-app

---
+++ 04-maps/maps-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { MapsApp } from './MapsApp';
+
+
+ReactDOM.render(
+    <MapsApp />,
+  document.getElementById('root')
+);
+
