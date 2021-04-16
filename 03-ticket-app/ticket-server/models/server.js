fa95cd62a39945fcd04526ca181e166dc639a51b
obbijuan <juan.garces@outlook.com>
2021-04-16 00:13:50 -0400

    REST service is added to obtain the latest tickets

--- 03-ticket-app/ticket-server/models/server.js
+++ 03-ticket-app/ticket-server/models/server.js
@@ -18,31 +18,38 @@ class Server {
         this.server = http.createServer( this.app );
         
         // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
+        this.io = socketio(this.server, { /* configuraciones */ });
+        
+        // Inicializar sockets
+        this.socket = new Sockets( this.io );
+        
     }
 
     middlewares() {
+
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
 
-        // CORS
+        // Configuracion CORS
         this.app.use( cors() );
+        
+        // Obtiene los ultimos tickets
+        this.app.get('/latest', (req, res) => {
+            
+            res.json({
+                ok: true,
+                latest: this.socket.ticketList.ultimos13
+            })
+        })
 
     }
 
-    // Esta configuración se puede tener aquí o como propieda de clase
-    // depende mucho de lo que necesites
-    configurarSockets() {
-        new Sockets( this.io );
-    }
+    
 
     execute() {
 
         // Inicializar Middlewares
         this.middlewares();
-
-        // Inicializar sockets
-        this.configurarSockets();
 
         // Inicializar Server
         this.server.listen( this.port, () => {
