b435c275994592b6a73b8e0a81e2782f75bd4d0f
obbijuan <juan.garces@outlook.com>
2021-04-16 23:52:14 -0400

    useRef in place of useState

--- 04-maps/maps-app/src/pages/MapPage.js
+++ 04-maps/maps-app/src/pages/MapPage.js
@@ -6,31 +6,54 @@ require('dotenv').config();
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const initPoint = {
-    lng: -70.620,
-    lat: -33.450,
-    zoom: 11
+    lng: -70.6410,
+    lat: -33.4424,
+    zoom: 11.45
 }
 
 export const MapPage = () => {
 
     const mapDiv = useRef();
-    const [, setMap] = useState();
+    // const [ map, setMap ] = useState();
+    const map = useRef();
+    const [ coords, setCoords ] = useState(initPoint)
 
 
     useEffect(() => {
         
-        const mapbox_lg = new mapboxgl.Map({
+        const mapbox = new mapboxgl.Map({
             container: mapDiv.current,
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [ initPoint.lng, initPoint.lat ],
             zoom: initPoint.zoom
         });
-        setMap( mapbox_lg );
+        
+        map.current = mapbox;
 
-    }, [])
+    }, []);
+
+
+
+    // Se dispara cuando se mueve el mapa
+    useEffect(() => {
+
+        map.current?.on('move', () => {
+            const { lng, lat } = map.current.getCenter();
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: map.current.getZoom().toFixed(2),
+            });
+        })
+
+    }, []);
+
 
     return (
         <>
+            <div className="info">
+                Lng: { coords.lng } | lat: { coords.lat } | zoom: { coords.zoom }
+            </div>
             <div
                 ref={ mapDiv }
                 className="mapContainer"
