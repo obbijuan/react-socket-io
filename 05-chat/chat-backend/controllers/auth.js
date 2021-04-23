938f94b0aab4dae49f7f0b8bba94dccd0771e849
obbijuan <juan.garces@outlook.com>
2021-04-22 23:27:02 -0400

    Encrypt the password

--- 05-chat/chat-backend/controllers/auth.js
+++ 05-chat/chat-backend/controllers/auth.js
@@ -1,12 +1,43 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/user');
 
 
 const createUser = async(req, res = response) => {
 
-    res.json({
-        ok: true,
-        usuario: 'obbijuan'
-    });
+    try {
+
+        const { email, password } = req.body;
+
+        const existEmail = await Usuario.findOne({ email });
+        if ( existEmail ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe!'
+            });
+        }
+
+        const usuario = new Usuario( req.body );
+
+        //Encriptar password
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
+
+        // Guardar usuario en DB
+        await usuario.save();
+
+        res.json({
+            usuario
+        })
+
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Contacte al administrador!'
+        })
+    }
 
 }
 
