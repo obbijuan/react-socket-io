7decc1f9a4ae01b13128d73e69979ef92de59cfc
obbijuan <juan.garces@outlook.com>
2021-05-06 00:01:11 -0400

    Definition of required socket events

--- 05-chat/chat-backend/models/sockets.js
+++ 05-chat/chat-backend/models/sockets.js
@@ -13,12 +13,22 @@ class Sockets {
         // On connection
         this.io.on('connection', ( socket ) => {
 
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
+            // TODO: Validar el JWT
+            // Si el token no es valido, desconectar
+
+            // TODO: Saber que el usuario está activo mediante el UID
+
+            // TODO: Emitir todos los usuarios conectados
+
+            // TODO: Socket join, uid
+
+            // TODO: Escuchar cuando el cliente manda un mensaje
+            // mensaje-personal
+
+            // TODO: Disconnect
+            // Marcar en la BD que el usuario se desconecto
+            
+            // TODO: Emitir todos los usuarios conectados
             
         
         });
