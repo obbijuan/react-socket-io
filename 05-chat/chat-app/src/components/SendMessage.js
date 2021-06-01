1230b715d2f6ac567cc10e5a90efde7cb0259db2
obbijuan <juan.garces@outlook.com>
2021-06-01 00:28:17 -0400

    Send messages - Part 1

--- 05-chat/chat-app/src/components/SendMessage.js
+++ 05-chat/chat-app/src/components/SendMessage.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const SendMessage = () => {
+
+    const [message, setMessage] = useState('');
+
+    const onChange = ({ target }) => {
+        setMessage( target.value );
+    }
+
+    const onSubmit = (ev) => {
+
+        ev.preventDefault();
+        
+        if (message.length === 0) { return; }
+        setMessage('');
+
+        // TODO: Emitir un evento de sockets para enviar el mensaje
+
+        // TODO: Hacer el dispatch del mensaje
+
+    }
+
     return (
-        <form>
+        <form onSubmit={ onSubmit }>
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..." />
+                    <input 
+                        type="text"
+                        className="write_msg"
+                        placeholder="Mensaje..."
+                        value={ message }
+                        onChange={ onChange }
+                    />
                 </div>
                 <div className="col-sm-3 text-center">
                     <button className="msg_send_btn mt-3" type="submit">
